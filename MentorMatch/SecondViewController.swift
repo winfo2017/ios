617d6133564f5c48851f710d32fc95d83f448924
MyungJin Eun <eunmyun@uw.edu>
2017-01-28 12:26:33 -0800

    stater

---
+++ MentorMatch/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MentorMatch
+//
+//  Created by MyungJin Eun on 1/28/17.
+//  Copyright © 2017 BabyShaker. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
